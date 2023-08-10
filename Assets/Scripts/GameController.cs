ee8c9db0488641004507a535749177cfb0c3b480
NeilHigginbotham <neil.t.higginbotham@gmail.com>
2023-08-10 06:43:12 -0400

    Card Update

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Card> deck = new List<Card>();
+    public Transform[] cardSlots;
+    public bool[] availableCardSlots;
     void Start()
     {
         
@@ -15,4 +18,15 @@ public class GameController : MonoBehaviour
     {
         
     }
+
+    public void DrawCard()
+    {
+        if (deck.Count >= 1)
+        {
+            Card randCard = deck[Random.Range(0, deck.Count)]
+        }
+    }
+
+
+
 }
