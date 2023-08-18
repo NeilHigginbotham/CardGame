ea013f484fb2cbba58d765c737d5fe313f59501d
NeilHigginbotham <neil.t.higginbotham@gmail.com>
2023-08-18 10:10:44 -0400

    Initial Draw Seven Method

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,17 +8,22 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public List<Card> deck = new List<Card>();
+    public List<CardDisplay> deck = new List<CardDisplay>();
     public Transform[] cardSlots;
     public bool[] availableCardSlots;
-    public Text deckSizeText;
+    public TextMeshProUGUI deckSizeText;
+
+    private void Start()
+    {
+        // DrawStartingCards(7); // Draw 7 cards at the start of the game
+    }
 
 
     public void DrawCard()
     {
         if (deck.Count >= 1)
         {
-            Card randCard = deck[Random.Range(0, deck.Count)];
+            CardDisplay randCard = deck[Random.Range(0, deck.Count)];
 
             for (int i = 0; i < availableCardSlots.Length; i++)
             {
@@ -33,11 +38,31 @@ public class GameController : MonoBehaviour
             }
         }
     }
-    private void Update()
+
+    /*
+    public void DrawStartingCards(int numCards)
     {
-        deckSizeText.text = deck.Count.ToString();
-    }
+        for (int drawCount = 0; drawCount < numCards; drawCount++)
+        {
+            if (deck.Count >= 1)
+            {
+                Card randCard = deck[Random.Range(0, deck.Count)];
+
+                for (int i = 0; i < availableCardSlots.Length; i++)
+                {
+                    if (availableCardSlots[i])
+                    {
+                        randCard.gameObject.SetActive(true);
+                        randCard.transform.position = cardSlots[i].position;
+                        availableCardSlots[i] = false;
+                        deck.Remove(randCard);
+                        break;
 
 
 
+                    } 
+                }
+            }
+        }
+    }*/
 }
---
+++ Assets/Scripts/PhaseManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class PhaseManager : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
