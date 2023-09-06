341ebfc06eacfaa7c4a0c102a8c25401cb92247c
NeilHigginbotham <59791812+NeilHigginbotham@users.noreply.github.com>
2023-09-06 18:25:59 -0400

    mana string increase update

--- Assets/Scripts/Land.cs
+++ Assets/Scripts/Land.cs
@@ -9,13 +9,15 @@ public class Land : MonoBehaviour, IPointerClickHandler
     public CardDisplay card;
 
     GameObject Player1Mana;
-    //public GameObject Player1Mana;
+    public Text player1manacounter;
+    private int player1manacount = 0;
     public bool isCardTappedLand = false;
 
 
     void Start()
     {
         Player1Mana = GameObject.Find("Player1Mana");
+        player1manacount = GameObject.Find("player1manacounter");
     }
 
     public void OnPointerClick(PointerEventData pointerEventData)
@@ -27,9 +29,16 @@ public class Land : MonoBehaviour, IPointerClickHandler
                 // If the land has been played and is untapped then we will make one mana.
                 Debug.Log("Make one mana");
                 isCardTappedLand = true;
-                Player1Mana.SetActive(false);
-
+                ManaGain();
             }
         }
+    }
+    public void ManaGain()
+    {
+        player1manacount++;
+    }
+    private void UpdateText()
+    {
+        player1manacounter.text = "player1mana" + player1manacount.ToString();
     }
 }
